9eb690e5ce347de3e3d4d7ae6b3a2d3cac68962a
Betül Tuğçe Dikdoğmuş <79140478+BetulTugce@users.noreply.github.com>
2021-10-04 23:19:44 +0300

    Added updates.

---
+++ BusinessLayer/ValidationRules_FluentValidation/CategoryValidatior.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules_FluentValidation
+{
+    public class CategoryValidatior : AbstractValidator<Category>
+    {
+        public CategoryValidatior()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adını boş bırakmayınız!");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Kategori adı en az 3 karakter uzunluğunda olmalıdır!");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Kategori adı 20 karakterden uzun olamaz!");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklamayı boş bırakmayınız!");
+        }
+    }
+}
